442b273e5265a362115d0768daaaadde1d09b3b4
Aoi <aoi.hosomi.218@gmail.com>
2023-08-26 16:40:09 +0900

    signin-hookを更新しました

--- src/hooks/user/useSignup.ts
+++ src/hooks/user/useSignup.ts
@@ -1,12 +1,39 @@
+import { useToast } from '@chakra-ui/react';
 import axios from 'axios';
+import useSWRMutation from 'swr/mutation';
 
-export const useSignup = (email: string, password: string, name: string) => {
-  const params = {
-    email,
-    password,
-    name,
-  };
-  return axios
-    .post(`${import.meta.env.VITE_APP_API_URL}/signup`, params)
-    .then((res) => console.log(res));
+type User = {
+  email: string;
+  password: string;
+  name: string;
+};
+
+const fetcher = async (user: User) => {
+  await axios.post(`${import.meta.env.VITE_APP_API_URL}/signup`, user);
+};
+
+export const useSignup = (user: User) => {
+  const toast = useToast();
+
+  return useSWRMutation('api/post/signup', () => fetcher(user), {
+    onSuccess: () => {
+      toast({
+        title: 'サインアップに成功しました。',
+        status: 'success',
+      });
+    },
+    onError: (e) => {
+      if (e instanceof Error) {
+        toast({
+          title: e.message,
+          status: 'error',
+        });
+      } else {
+        toast({
+          title: 'サインアップに失敗しました。',
+          status: 'error',
+        });
+      }
+    },
+  });
 };

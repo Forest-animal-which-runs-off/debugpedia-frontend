b39f02ee8e758f1326c608f257ce7d91203aaa3c
snaskn <0zu3851p1-9196e@ezweb.ne.jp>
2023-08-26 11:30:12 +0900

    feat:画像ファイルを取り込み、ヘッダーに表示させました

---
+++ src/header.tsx
@@ -0,0 +1,9 @@
+import { Box, Image } from '@chakra-ui/react';
+
+export const Header = () => {
+  return (
+    <Box backgroundColor='#5aec16' width='full'>
+      <Image src='debugpedia-frontend\public\Group_4.svg' />
+    </Box>
+  );
+};

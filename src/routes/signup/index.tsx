5f0dbe9ccde39fd2fdd8ae00bc1e793f44acf1fc
satojin219 <renzuoteng142@gmail.com>
2023-08-26 23:07:57 +0900

    feat: signup state

--- src/routes/signup/index.tsx
+++ src/routes/signup/index.tsx
@@ -1,6 +1,13 @@
-import { Button, Center, Flex, Image, Input, VStack } from '@chakra-ui/react';
+import { Button, Center, Flex, Image, Input, Link, VStack } from '@chakra-ui/react';
+import { useState } from 'react';
 
 export const Signup = () => {
+  const [userName, setUserName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleSubmit = () => {};
+
   return (
     <Center bg='#5AEC16' h='100vh' w='100vw'>
       <Center
@@ -12,20 +19,41 @@ export const Signup = () => {
       >
         <VStack w={{ base: '90%', md: '60%', lg: '50%' }}>
           <Image src='/debugpedia_logo.svg' alt='デバッグのロゴ' w='250px' mb='20px' />
-          <Input variant='flushed' placeholder='ユーザー名を入力して下さい' mb='4px' />
-          <Input variant='flushed' placeholder='メールアドレスを入力して下さい' mb='4px' />
-          <Input variant='flushed' placeholder='パスワードを入力して下さい' mb='12px' />
+          <Input
+            variant='flushed'
+            placeholder='ユーザー名を入力して下さい'
+            mb='4px'
+            value={userName}
+            onChange={(e) => setUserName(e.currentTarget.value)}
+          />
+          <Input
+            type='email'
+            variant='flushed'
+            placeholder='メールアドレスを入力して下さい'
+            mb='4px'
+            value={email}
+            onChange={(e) => setEmail(e.currentTarget.value)}
+          />
+          <Input
+            type='password'
+            variant='flushed'
+            placeholder='パスワードを入力して下さい'
+            mb='12px'
+            value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+          />
           <Flex
             direction={{ base: 'column', md: 'row' }}
             gap='8px'
             w='full'
             justify='space-between'
+            align='center'
             mb={{ base: '24px', md: '48px' }}
           >
-            <Button bg='none' color='blue.400' fontWeight='thin' px='32px'>
+            <Link href='/login' bg='none' color='blue.400' fontWeight='thin' px='32px'>
               ログイン画面へ
-            </Button>
-            <Button bg='#5AEC16' color='white' px='32px'>
+            </Link>
+            <Button bg='#5AEC16' color='white' px='32px' onClick={handleSubmit}>
               新規作成
             </Button>
           </Flex>

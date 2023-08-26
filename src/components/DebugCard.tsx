5c618d58a3511b523035aec4e3f14f87c4990fa2
はる <haru@harunoMacBook-Air.local>
2023-08-26 16:48:43 +0900

    feat: DebugCardのコンポーネントを実装

--- src/components/DebugCard.tsx
+++ src/components/DebugCard.tsx
@@ -11,57 +11,58 @@ type Props = {
 };
 
 export const DebugCard: FC<Props> = (props) => {
-  const debugCard = {
-    id: props.id,
-    title: props.title,
-    details: props.detail,
-    tech: props.tech,
-    author: props.author,
-  };
-
-  const debugCardLink = `/Debug/${debugCard.id}`;
+  const debugCardLink = `/Debug/${props.id}`;
 
   return (
     <Link to={debugCardLink}>
-    <Box bg='white' w='200px' borderRadius='lg' h='240' boxShadow='lg' p={5} color='black'  transition='transform 0.3s ease-in-out' // スケール変化にトランジションを追加
+      <Box
+        bg='white'
+        w='200px'
+        borderRadius='lg'
+        h='240'
+        boxShadow='lg'
+        p={5}
+        color='black'
+        transition='transform 0.3s ease-in-out' // スケール変化にトランジションを追加
         _hover={{
           transform: 'scale(1.1)', // ホバー時のスケール変化
-        }}>
-      <Box noOfLines={3} mb='8px' fontWeight='bold'>
-        <Text h={83} fontSize='18px'>
-          {debugCard.title}
-        </Text>
-      </Box>
-
-      <Text fontSize='15px' h={76}>
-        <Box mb='8px' noOfLines={3}>
-          {debugCard.details}
-        </Box>
-      </Text>
-
-      <Grid
-        templateAreas={`"nav main"
-                        "nav footer"`}
+        }}
       >
-        <GridItem area={'main'} pl='2'>
-          <Box bg='gray.300' color='white' borderRadius='15' p='1'>
-            <Text h='150' as='b' fontSize={13}>
-              <p>{debugCard.tech}</p>
-            </Text>
-          </Box>
-        </GridItem>
-
-        <GridItem area={'footer'} pl='2'>
-          <Text mr='-14' fontSize='14px'>
-            <>{debugCard.author}</>
+        <Box noOfLines={3} mb='8px' fontWeight='bold'>
+          <Text h={83} fontSize='18px'>
+            {props.title}
           </Text>
-        </GridItem>
+        </Box>
 
-        <GridItem pl='0' area={'nav'}>
-          <Image ml='1' mt='2' src='public/bug.svg' alt='Debugpedia' />
-        </GridItem>
-      </Grid>
-    </Box>
+        <Text fontSize='15px' h={76}>
+          <Box mb='8px' noOfLines={3}>
+            {props.detail}
+          </Box>
+        </Text>
+
+        <Grid
+          templateAreas={`"nav main"
+                        "nav footer"`}
+        >
+          <GridItem area={'main'} pl='2'>
+            <Box bg='gray.300' color='white' borderRadius='15' p='1'>
+              <Text h='150' as='b' fontSize={13}>
+                <p>{props.tech}</p>
+              </Text>
+            </Box>
+          </GridItem>
+
+          <GridItem area={'footer'} pl='2'>
+            <Text mr='-14' fontSize='14px'>
+              <>{props.author}</>
+            </Text>
+          </GridItem>
+
+          <GridItem pl='0' area={'nav'}>
+            <Image ml='1' mt='2' src='public/bug.svg' alt='Debugpedia' />
+          </GridItem>
+        </Grid>
+      </Box>
     </Link>
   );
 };

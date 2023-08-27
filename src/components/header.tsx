7a4fa6303fec986655d906aad3b33f5ee912edc2
snaskn <0zu3851p1-9196e@ezweb.ne.jp>
2023-08-28 06:45:04 +0900

    feat:ヘッダーを固定し、componentフォルダに移動しました

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+import { Box, Image } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+  return (
+    <Box as='header' width='100%' top={0} backgroundColor='#5aec16' p={4} position='sticky'>
+      <Box as={Link} display={'block'} width='fit-content' to='/login' >
+        <Image src='./Group_4.svg' />
+      </Box>
+    </Box>
+  );
+};

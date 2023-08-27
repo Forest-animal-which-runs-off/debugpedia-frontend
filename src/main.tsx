2f1323cae33f55899b1d3e29aed8816875d2eafc
satojin219 <renzuoteng142@gmail.com>
2023-08-27 13:40:21 +0900

    style: remove style.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
-import './index.css';
 import { ErrorPage } from './error-page.tsx';
 
 const router = createBrowserRouter([

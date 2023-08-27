da9f5bcc1c6a66e145907d5a4277756d904ec1a5
はる <haru@harunoMacBook-Air.local>
2023-08-27 20:08:39 +0900

    動作確認後、UIなどを変更しました

--- src/components/TechsInput.tsx
+++ src/components/TechsInput.tsx
@@ -1,18 +1,26 @@
 import { CloseIcon } from '@chakra-ui/icons';
-import { Box, Button, Input, VStack, Stack, Text, HStack } from '@chakra-ui/react';
-import { useState } from 'react';
+import { Box, Button, Input, VStack, Stack, Text, HStack, Flex } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
 type Props = {
   techs: string[];
-  onChange: (newTechs: string[]) => void;
+  setDebug: (newTechs: string[]) => void;
   isEdit: boolean;
 };
 
 function TechInput(props: Props) {
-  const [techs, setTechs] = useState([{ text: '' }]);
+  const [techs, setTechs] = useState(props.techs);
+
+  useEffect(() => {
+    if (!props.isEdit) {
+      const updatedTechs = techs.filter((tech) => tech !== '');
+      props.setDebug(techs);
+      setTechs(updatedTechs);
+    }
+  }, [props.isEdit]);
 
   const addTech = () => {
-    setTechs([...techs, { text: '' }]);
+    setTechs((prev) => [...prev, '']);
   };
 
   const removeTech = (index: number) => {
@@ -22,32 +30,37 @@ function TechInput(props: Props) {
 
   const handleTechChange = (index: number, value: string) => {
     const updatedTechs = [...techs];
-    updatedTechs[index].text = value;
+    updatedTechs[index] = value;
     setTechs(updatedTechs);
+    props.setDebug(techs);
   };
 
   return (
     <Box bg='white' w='1050px' borderRadius='lg' boxShadow='lg' p={5} color='black'>
-      <VStack spacing='8px' align='flex-start'>
-        <Stack direction='row' width='100%'>
-          <Text fontWeight='bold'>プログラミング言語/ライブラリ/フレームワーク</Text>
-          {props.isEdit && (
-            <Button fontWeight='bold' size='sm' h='6' bg='#5AEC16' color='white' onClick={addTech}>
-              ＋技術を追加
-            </Button>
-          )}
-        </Stack>
+      <Stack direction='row' width='100%'>
+        <Text fontWeight='bold'>プログラミング言語/ライブラリ/フレームワーク</Text>
+        {props.isEdit && (
+          <Button fontWeight='bold' size='sm' h='6' bg='#5AEC16' color='white' onClick={addTech}>
+            ＋技術を追加
+          </Button>
+        )}
+      </Stack>
+      
+
+      <Flex direction={props.isEdit ? "column":"row" } align='flex-start' justify='flex-start'>
         {techs.map((tech, index) => (
-          <HStack key={index} align='center'>
+          <HStack key={index} align='center' width='6%'>
             {props.isEdit ? (
               <>
-                <Input
-                  width='900px'
-                  placeholder='技術を入力'
-                  value={tech.text}
-                  onChange={(e) => handleTechChange(index, e.target.value)}
-                  disabled={!props.isEdit}
-                />
+                <VStack align='flex-start' pt='2'>
+                  <Input
+                    width='900px'
+                    placeholder='技術を入力'
+                    value={tech}
+                    onChange={(e) => handleTechChange(index, e.target.value)}
+                    disabled={!props.isEdit}
+                  />
+                </VStack>
                 {props.isEdit && (
                   <Button variant='ghost' size='sm' onClick={() => removeTech(index)}>
                     <CloseIcon />
@@ -57,13 +70,13 @@ function TechInput(props: Props) {
             ) : (
               <Box fontWeight='bold' bg='gray.400' color='white' borderRadius='15' p='1'>
                 <Text fontSize={16} ml='4' mr='4'>
-                  {tech.text}
+                  {tech}
                 </Text>
               </Box>
             )}
           </HStack>
         ))}
-      </VStack>
+      </Flex>
     </Box>
   );
 }

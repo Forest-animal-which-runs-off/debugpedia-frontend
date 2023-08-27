08823de322a0c899d101beb1970ba632c61101ef
はる <haru@harunoMacBook-Air.local>
2023-08-27 09:39:26 +0900

    linkInputの実装

--- src/components/linkInput.tsx
+++ src/components/linkInput.tsx
@@ -1,19 +1,18 @@
-import { CloseIcon } from "@chakra-ui/icons";
-import { Box, Button, VStack, Stack, HStack, Text, Input } from "@chakra-ui/react";
-import { useState } from "react";
+import { CloseIcon } from '@chakra-ui/icons';
+import { Box, Button, VStack, Stack, HStack, Text, Input } from '@chakra-ui/react';
+import { useState } from 'react';
 
 type Props = {
-    links: string[];
-    onChange: () => void;
-    isEdit: boolean;
-  };
-  
+  links: string[];
+  onChange: () => void;
+  isEdit: boolean;
+};
 
 function LinkInput(props: Props) {
-  const [links, setLinks] = useState(props.links.map(link => ({ link })));
+  const [links, setLinks] = useState(props.links.map((link) => ({ link })));
 
   const addLink = () => {
-    setLinks([...links, { link: "" }]);
+    setLinks([...links, { link: '' }]);
   };
 
   const removeLink = (index: number) => {
@@ -28,29 +27,29 @@ function LinkInput(props: Props) {
   };
 
   return (
-    <Box bg="white" w="1050px" borderRadius="lg" boxShadow="lg" p={5} color="black">
-      <VStack spacing="8px" align="flex-start">
-        <Stack direction="row" width="100%">
-          <Text fontWeight="bold">参考にした記事、リンク一覧</Text>
+    <Box bg='white' w='1050px' borderRadius='lg' boxShadow='lg' p={5} color='black'>
+      <VStack spacing='8px' align='flex-start'>
+        <Stack direction='row' width='100%'>
+          <Text fontWeight='bold'>参考にした記事、リンク一覧</Text>
           {props.isEdit && (
-            <Button fontWeight="bold" size="sm" h="6" bg="#5AEC16" color="white" onClick={addLink}>
+            <Button fontWeight='bold' size='sm' h='6' bg='#5AEC16' color='white' onClick={addLink}>
               ＋リンクを追加
             </Button>
           )}
         </Stack>
         {links.map((link, index) => (
-          <HStack key={index} align="center">
+          <HStack key={index} align='center'>
             {props.isEdit ? (
               <>
                 <Input
-                  width="900px"
-                  placeholder="リンク、記事を入力"
+                  width='900px'
+                  placeholder='リンク、記事を入力'
                   value={link.link}
                   onChange={(e) => handleLinkChange(index, e.target.value)}
                   disabled={!props.isEdit}
                 />
                 {props.isEdit && (
-                  <Button variant="ghost" size="sm" onClick={() => removeLink(index)}>
+                  <Button variant='ghost' size='sm' onClick={() => removeLink(index)}>
                     <CloseIcon />
                   </Button>
                 )}

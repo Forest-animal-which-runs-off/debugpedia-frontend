5e957d6cbc4fb6fd42ade50f7b87710784313b69
はる <haru@harunoMacBook-Air.local>
2023-08-27 10:38:36 +0900

    linkInputの実装

--- src/components/linkInput.tsx
+++ src/components/linkInput.tsx
@@ -55,7 +55,7 @@ function LinkInput(props: Props) {
                 )}
               </>
             ) : (
-              <Text>{link.link}</Text>
+              <link>{link.link}</link>
             )}
           </HStack>
         ))}

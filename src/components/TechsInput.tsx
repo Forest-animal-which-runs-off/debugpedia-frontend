fba18162f0491d9eec2d08d3b66d09732cb8a778
はる <haru@harunoMacBook-Air.local>
2023-08-27 11:59:22 +0900

    TechsInputの実装

--- src/components/TechsInput.tsx
+++ src/components/TechsInput.tsx
@@ -1,17 +1,18 @@
 import { CloseIcon } from '@chakra-ui/icons';
-import { Box, Button, Input, VStack, Stack, Text, HStack } from "@chakra-ui/react";
-import { useState } from "react";
+import { Box, Button, Input, VStack, Stack, Text, HStack } from '@chakra-ui/react';
+import { useState } from 'react';
 
 type Props = {
   techs: string[];
-  onChange: (newTechs: string[]) => void;   isEdit: boolean;
+  onChange: (newTechs: string[]) => void;
+  isEdit: boolean;
 };
 
 function TechInput(props: Props) {
-  const [techs, setTechs] = useState([{ text: "" }]);
+  const [techs, setTechs] = useState([{ text: '' }]);
 
   const addTech = () => {
-    setTechs([...techs, { text: "" }]);
+    setTechs([...techs, { text: '' }]);
   };
 
   const removeTech = (index: number) => {
@@ -26,36 +27,38 @@ function TechInput(props: Props) {
   };
 
   return (
-    <Box bg="white" w="1050px" borderRadius="lg" boxShadow="lg" p={5} color="black">
-      <VStack spacing="8px" align="flex-start">
-        <Stack direction="row" width="100%">
-          <Text fontWeight="bold">プログラミング言語/ライブラリ/フレームワーク</Text>
+    <Box bg='white' w='1050px' borderRadius='lg' boxShadow='lg' p={5} color='black'>
+      <VStack spacing='8px' align='flex-start'>
+        <Stack direction='row' width='100%'>
+          <Text fontWeight='bold'>プログラミング言語/ライブラリ/フレームワーク</Text>
           {props.isEdit && (
-            <Button fontWeight="bold" size="sm" h="6" bg="#5AEC16" color="white" onClick={addTech}>
+            <Button fontWeight='bold' size='sm' h='6' bg='#5AEC16' color='white' onClick={addTech}>
               ＋技術を追加
             </Button>
           )}
         </Stack>
         {techs.map((tech, index) => (
-          <HStack key={index} align="center">
+          <HStack key={index} align='center'>
             {props.isEdit ? (
               <>
                 <Input
-                  width="900px"
-                  placeholder="技術を入力"
+                  width='900px'
+                  placeholder='技術を入力'
                   value={tech.text}
                   onChange={(e) => handleTechChange(index, e.target.value)}
                   disabled={!props.isEdit}
                 />
                 {props.isEdit && (
-                  <Button variant="ghost" size="sm" onClick={() => removeTech(index)}>
+                  <Button variant='ghost' size='sm' onClick={() => removeTech(index)}>
                     <CloseIcon />
                   </Button>
                 )}
               </>
             ) : (
-                <Box fontWeight='bold' bg='gray.400' color='white' borderRadius='15' p='1'>
-              <Text fontSize={16} ml="4" mr="4">{tech.text}</Text>
+              <Box fontWeight='bold' bg='gray.400' color='white' borderRadius='15' p='1'>
+                <Text fontSize={16} ml='4' mr='4'>
+                  {tech.text}
+                </Text>
               </Box>
             )}
           </HStack>

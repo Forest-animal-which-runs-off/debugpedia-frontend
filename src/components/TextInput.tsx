cb2c63011455a65c70b35c79a36543582a5addf4
Aoi <aoi.hosomi.218@gmail.com>
2023-08-27 14:34:04 +0900

    UIを完成させた。要修正かも

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,27 +1,41 @@
+import { Box, Text, Textarea } from '@chakra-ui/react';
+
 type Props = {
   label: string;
   value: string;
-  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onChange: (value: string) => void;
   isEdit: boolean;
 };
 
 const TextInput = (props: Props) => {
   const { label, value, onChange, isEdit } = props;
 
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    onChange(e.target.value);
+  };
+
   return (
-    <>
+    <div>
       {isEdit ? (
-        <>
-          <label>{label}</label>
-          <textarea value={value} onChange={onChange} />
-        </>
+        <Box bg='white' w='100%' borderRadius='lg' boxShadow='lg' color='block' p={5} m={5}>
+          <Text fontWeight='bold' display='flex' ml='3px' p={3}>
+            {label}
+          </Text>
+          <Textarea value={value} onChange={handleChange} border='1px solid black' />
+        </Box>
       ) : (
         <>
-          <p>{label}</p>
-          <p>{value}</p>
+          <Box bg='white' w='100%' borderRadius='lg' boxShadow='lg' color='block' p={5} m={5}>
+            <Text fontWeight='bold' display='flex' ml='3px' p={3}>
+              {label}
+            </Text>
+            <Text display='flex' ml='3px' textAlign='left' minH='80px' p='8px 16px'>
+              {value}
+            </Text>
+          </Box>
         </>
       )}
-    </>
+    </div>
   );
 };
 
